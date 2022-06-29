3a9371cec01a6fd12aee048be3b597282deebe02
levifleal <levifleal@Live.com>
2022-06-29 12:45:07 -0300

    a

---
+++ exercicios/ex006.js
@@ -0,0 +1,7 @@
+var vel = 12
+console.log(`a velocidade do seu carro é ${vel}Km/h`)
+if(vel > 60){
+    console.log(`voce utrapasou a velocidade permitida, MULTADO`)
+}
+
+console.log(`diriga com cuidado`)
